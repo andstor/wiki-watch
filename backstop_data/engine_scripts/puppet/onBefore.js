b8bb29bcf295a3219fe5bf7ea583a4ed4a7b711c
André Storhaug <andr3.storhaug@gmail.com>
2020-10-14 02:44:55 +0200

    Update onBefore.js

--- backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -3,8 +3,8 @@ module.exports = async (page, scenario, vp) => {
   //do whatever is related to the login process
   await page.goto('https://ntnu.blackboard.com');
   await page.$eval('#altLogin', elem => elem.click());
-  await page.type('#user_id', 'process.env.USERNAME');
-  await page.type('#password', 'process.env.PASSWORD');
+  await page.type('#user_id', 'process.env.BB_USERNAME');
+  await page.type('#password', 'process.env.BB_PASSWORD');
   await page.$eval('#entry-login', elem => elem.click());
   await page.waitFor(3000);
 };
