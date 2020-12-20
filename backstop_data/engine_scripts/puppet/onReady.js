194acab9cb3e5799d728d1dc731a6a3d9d48fbfb
André Storhaug <andr3.storhaug@gmail.com>
2020-12-20 04:49:12 +0100

    Update onReady.js

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,8 +2,11 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
+  /*
   await page.waitForNavigation({
     waitUntil: 'networkidle0',
   });
+  */
+  
   // add more ready handlers here...
 };
